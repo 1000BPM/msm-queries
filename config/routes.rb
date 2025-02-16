e898922da92f891ceebd5c3877aceec54204effb
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-16 04:37:41 +0000

    Left note about where to resume

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index"})
 
   get("/directors/:the_id", {:controller => "directors", :action => "show"})
+
+  #left off before finishing director filmography - see video @28:00
 end
